32ee60a002f7ec67109ddd04ff48099eaedca105
Vikas Chaudhary <vikaschaudhary2309@gmail.com>
2024-06-17 12:56:41 +0530

    Update config.py

--- Voice Assistant/config.py
+++ Voice Assistant/config.py
@@ -1,4 +1,4 @@
 
-weather_api_key = "50d088434d7ca590881e64474d050c8b"  # Add your OpenWeatherMap API key here
+weather_api_key = ""  # Add your OpenWeatherMap API key here
 
-news_api_key = "1d75386bf8cc4f98a476d37368207e8b"  # Add your NewsAPI.org API key here
+news_api_key = ""  # Add your NewsAPI.org API key here
